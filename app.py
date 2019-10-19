b72118609efd902d546a30ba2bf3f1ad33f3c867
Charles Rule <cruleis@gmail.com>
2019-10-19 12:28:33 -0400

    mongoDB connection

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ from flask import Flask, render_template, request
 
 from pymongo import MongoClient
 
+client = MongoClient("mongodb+srv://admin:HACKRUpassword@cluster0-d2rxd.gcp.mongodb.net/test?retryWrites=true&w=majority")
+
+db = client.test
